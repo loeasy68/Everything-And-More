b946261f9c31e391f4286dceb64f0cf4f17e509e
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2024-09-30 01:54:10 +0000

    Started python version, NOTE: Is going to be pain to refactor

---
+++ main.py
@@ -0,0 +1,21 @@
+# Codes for everything
+#  D - Decomposition
+#  pe - Plastic Eating
+#  b - Bacteria
+#  cb - Comporter Box
+#  w - Worms
+# Example code would look like "Dpe"
+class Decomposition:
+    def __init__(self, age, type):
+        self.age = age
+        self.type = tyoe
+    def run():
+        # This is an If tree based on each type
+        if self.type == "Dpe":
+            pass
+        elif self.type == "Db":
+            pass
+        elif self.type == "Dcb":
+            pass
+        elif self.type == "Dw":
+            pass
