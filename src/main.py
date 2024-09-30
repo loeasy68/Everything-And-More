5698a5bd7a8fd06921c26b9e6d10da98395f2420
bobboi <110208193+901238746@users.noreply.github.com>
2024-09-30 01:58:52 +0000

    Added some placeholder classes and added a mod init

---
+++ src/main.py
@@ -0,0 +1,15 @@
+class mod:
+    def __init__(self) -> None:
+        self.name = "everything-and-more"
+        self.authors = ["Bob", "Loeasy"]
+        self.version = "1.21"
+        self.java = "21"
+    class storage():
+        #Insert chests
+        def chestidk():
+            print("Sample Text")
+    class decomposition:
+        def __init__(self, age, type):
+            self.age = age
+            self.type = type
+        #Insert Decompisition
