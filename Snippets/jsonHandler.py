1f3e45ce745e0718aa12e361b193170c67279596
bobboi <110208193+901238746@users.noreply.github.com>
2024-10-02 19:02:44 +0000

    Finished Json. Updated Json test
    Co-authored-by: loeasy68 <loeasy68@users.noreply.github.com>

--- Snippets/jsonHandler.py
+++ Snippets/jsonHandler.py
@@ -1,6 +1,9 @@
 import json
-jsonIDS = open("jsonIDS.json", "r")
+jsonIDS = open(".././src/jsonIDS.json", "r")
 jsoncontents = jsonIDS.read()
 jsonstuff = json.loads(jsoncontents)
 #This is the syntax jsonstuff["DNAs"][0]
-print(jsonstuff["DNAs"][18])
+#print(jsonstuff["DNAs"][18])
+#Prints all side effects
+for i in jsonstuff["SideEffects"]:
+    print(i)
